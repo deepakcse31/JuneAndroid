deb76bb34f39cc6cee6d6b40d45a495c1ff03faf
Deepak <deepakcse31@gmail.com>
2025-06-13 21:18:41 +0530

    Custom Dialog/Visibility

---
+++ app/src/main/java/com/example/juneandroid/VisibilityActivity.java
@@ -0,0 +1,40 @@
+package com.example.juneandroid;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class VisibilityActivity extends AppCompatActivity {
+    Button btnvisibility;
+    ImageView ivlogo;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_visibility);
+        ivlogo=findViewById(R.id.ivlogo);
+        btnvisibility=findViewById(R.id.btnvisibility);
+
+        btnvisibility.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ivlogo.setVisibility(View.VISIBLE);
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
